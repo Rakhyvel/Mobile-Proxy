33ff9953ef0d4dba7f5891c68d1669753cb77a07
Aj <Ajconnick@outlook.com>
2022-02-11 12:27:34 +0000

    updated server

--- main.c
+++ main.c
@@ -1,11 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+/*
+*Creats and returns socket
+*/
+int build_socket(){
+    int sock;
+    sock = socket (PF_INET, SOCK_STREAM,0); // using stream for TCP
+    if(sock < 0){
+        fprintf(stderr,"Unable to create socket");
+        exit(1);
+    }
+    return sock;
+}
 
 /* 
  * Awaits a connection, reads in packet including header and payload, prints out payload
  */
 void server() {
+    unsigned int buffer_size = 255;
+   // unsigned int buff_pos = 0;
+    char* buff = calloc(buffer_size,sizeof(char));
+    int acc,b,l,sock;
+    //socket in for inet only
+    struct sockaddr_in serverAddr, clientAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(8080);// added local to hold spot 
+    sock = build_socket();
+//still need to fill out serverAddr
+    b = bind(sock,(struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if(b < 0){
+	fprintf(stderr,"Unable to Bind");
+        exit(1);
+    }
+    l = listen(sock,5);// pending connections on socket
+    if(l < 0){
+        fprintf(stderr,"Unable to Listen");
+        exit(1);
+    }
+    while(1){//read infintly
+        //accept the conection
+        socklen_t client_len;
+        client_len  = sizeof(clientAddr);
+        acc = accept(sock,(struct sockaddr *)&clientAddr,&client_len);
+        if(acc < 0){
+           fprintf(stderr,"Unable to accept connection");
+           exit(1);
+        }
+        int len;
+        while((len = recv(acc,buff,sizeof(buff),0))){//read in from client
+
+       }
+
+    }
+
+   free(buff);
 
 }
 
