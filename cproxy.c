8906d24accab9e708471f17fdfed123db20032bf
Rakhyvel <josephmshimel@gmail.com>
2022-02-27 18:19:36 -0700

    close

--- cproxy.c
+++ cproxy.c
@@ -44,41 +44,42 @@ void cproxy(int port, char* ipText , char* portText) {
     int MAX_LEN = 1024;
     struct sockaddr_in sproxyAddr; // address of sproxy
     struct sockaddr_in cproxyAddr; // self address
-    struct sockaddr_in telnetAddr; // address for telnet connection
+    struct sockaddr_in telnetAddr; // address for telnet connection    
 
-    // Connect to sproxy
-    int sproxySock = socket(PF_INET, SOCK_STREAM, 0);
-    if (sproxySock == -1) {
-        perror("client failed creating socket");
-        exit(1);
-    }
-    sproxyAddr.sin_family = AF_INET;
-    sproxyAddr.sin_port = htons(atoi(portText));
-    inet_pton(AF_INET, ipText, &sproxyAddr.sin_addr);
-    if (connect(sproxySock, (struct sockaddr*)&sproxyAddr, sizeof(sproxyAddr)) == -1) {
-        perror("client failed connecting socket");
-        exit(1);
-    }
+    int closed = 0;
+    while (!closed) {
+        // Create telnet socket
+        int telnetSock = socket(PF_INET, SOCK_STREAM, 0);
+        cproxyAddr.sin_family = AF_INET;
+        cproxyAddr.sin_addr.s_addr = INADDR_ANY; // htonl INADDR_ANY ;
+        cproxyAddr.sin_port = htons(port); // added local to hold spot 
+        if (telnetSock < 0) {
+            fprintf(stderr,"Unable to create socket");
+            exit(1);
+        }
+        if (bind(telnetSock, (struct sockaddr*)&cproxyAddr, sizeof(cproxyAddr)) < 0) {
+            fprintf(stderr,"Unable to Bind");
+            exit(1);
+        }
+        if (listen(telnetSock, 5) < 0) {
+            fprintf(stderr,"Unable to Listen");
+            exit(1);
+        }
 
-    // Create telnet socket
-    int telnetSock = socket(PF_INET, SOCK_STREAM, 0);
-    cproxyAddr.sin_family = AF_INET;
-    cproxyAddr.sin_addr.s_addr = INADDR_ANY; // htonl INADDR_ANY ;
-    cproxyAddr.sin_port = htons(port); // added local to hold spot 
-    if (telnetSock < 0) {
-        fprintf(stderr,"Unable to create socket");
-        exit(1);
-    }
-    if (bind(telnetSock, (struct sockaddr*)&cproxyAddr, sizeof(cproxyAddr)) < 0) {
-        fprintf(stderr,"Unable to Bind");
-        exit(1);
-    }
-    if (listen(telnetSock, 5) < 0) {
-        fprintf(stderr,"Unable to Listen");
-        exit(1);
-    }
+        // Connect to sproxy
+        int sproxySock = socket(PF_INET, SOCK_STREAM, 0);
+        if (sproxySock == -1) {
+            perror("client failed creating socket");
+            exit(1);
+        }
+        sproxyAddr.sin_family = AF_INET;
+        sproxyAddr.sin_port = htons(atoi(portText));
+        inet_pton(AF_INET, ipText, &sproxyAddr.sin_addr);
+        if (connect(sproxySock, (struct sockaddr*)&sproxyAddr, sizeof(sproxyAddr)) == -1) {
+            perror("client failed connecting socket");
+            exit(1);
+        }
 
-    while (1) {
         // Accept new telnet connection
         socklen_t telnetLen;
         telnetLen  = sizeof(telnetAddr);
@@ -120,12 +121,16 @@ void cproxy(int port, char* ipText , char* portText) {
                 memset(buff2, 0, MAX_LEN);
                 int rev2 = recv(sproxySock, buff2, MAX_LEN, 0);
                 if (rev2 <= 0) {
+                    closed = 1;
                     break;
                 }
                 printf("sproxy data: %s\n", buff2);
                 send_data(telnetCon, buff2, rev2);
             }
         }
+        close(sproxySock);
+        close(telnetCon);
+        close(telnetSock);
     }
 }
 
